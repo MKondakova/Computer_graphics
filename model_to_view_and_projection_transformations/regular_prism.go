4a5b88dfd58c9b88d6753d125f2e0d8cf0251723
MKondakova <milanon@ya.ru>
2021-03-29 23:41:14 +0300

    Добавлена изометрическая проекция

--- model_to_view_and_projection_transformations/regular_prism.go
+++ model_to_view_and_projection_transformations/regular_prism.go
@@ -10,17 +10,26 @@ import (
 )
 
 const SIZE = 600
+const HEIGHT = 0.5
 
-var RADIUS float64 = math.Sqrt(0.5)
-var HEIGHT float64 = 0.5
+var isometricProjectionMatrix []float64 = []float64{
+
+	math.Cos(phi), 0.0, math.Sin(phi), 0.0,
+	math.Sin(phi) * math.Sin(theta), math.Cos(theta), -1 * math.Cos(phi) * math.Sin(theta), 0.0,
+	math.Sin(phi) * math.Cos(theta), -1 * math.Sin(theta), -1 * math.Cos(phi) * math.Cos(theta), 0.0,
+	0.0, 0.0, 0.0, 1.0,
+}
+
+var theta float64 = 45 * math.Pi / 180.0
+var phi float64 = 35.26 * math.Pi / 180.0
+var RADIUS float64 = math.Sqrt(0.5) * 0.5
 
 func drowBase(vertexes [][2]float64, z float64) {
 
 	gl.Begin(gl.POLYGON)
-	gl.Color3d(z, 0.2, 0.2)
+	gl.Color3d(z, 1, 1)
 	for _, vertex := range vertexes {
 		gl.Vertex3d(vertex[0], vertex[1], z)
-		log.Println(vertex[0], vertex[1], z)
 	}
 	gl.End()
 }
@@ -28,11 +37,11 @@ func drowBase(vertexes [][2]float64, z float64) {
 func drowSideFaces(vertexes [][2]float64, height float64) {
 	for i := 0; i < len(vertexes); i++ {
 		gl.Begin(gl.QUADS)
-		gl.Color3d(0.5, 0.3, float64(i)/5)
-		gl.Vertex3d(vertexes[i][0], vertexes[i][1], 0)
-		gl.Vertex3d(vertexes[i][0], vertexes[i][1], height)
-		gl.Vertex3d(vertexes[(i+1)%len(vertexes)][0], vertexes[(i+1)%len(vertexes)][1], height)
-		gl.Vertex3d(vertexes[(i+1)%len(vertexes)][0], vertexes[(i+1)%len(vertexes)][1], 0)
+		gl.Color3d(0.2, 0.2, float64(i)/3)
+		gl.Vertex3d(vertexes[i][0], vertexes[i][1], height/-2)
+		gl.Vertex3d(vertexes[i][0], vertexes[i][1], height/2)
+		gl.Vertex3d(vertexes[(i+1)%len(vertexes)][0], vertexes[(i+1)%len(vertexes)][1], height/2)
+		gl.Vertex3d(vertexes[(i+1)%len(vertexes)][0], vertexes[(i+1)%len(vertexes)][1], height/-2)
 		gl.End()
 	}
 
@@ -42,12 +51,12 @@ func drowPrism(n int) {
 	vertexes := [][2]float64{}
 	for i := 0; i < n; i++ {
 		vertexes = append(vertexes, [2]float64{
-			RADIUS * math.Cos(2*math.Pi*float64(i)/float64(n)),
-			RADIUS * math.Sin(2*math.Pi*float64(i)/float64(n)),
+			RADIUS * math.Cos(2*math.Pi*float64(i)/float64(n)+math.Pi*45/180),
+			RADIUS * math.Sin(2*math.Pi*float64(i)/float64(n)+math.Pi*45/180),
 		})
 	}
-	drowBase(vertexes, 0)
-	drowBase(vertexes, HEIGHT)
+	drowBase(vertexes, HEIGHT/-2)
+	drowBase(vertexes, HEIGHT/2)
 	drowSideFaces(vertexes, HEIGHT)
 }
 
@@ -88,11 +97,13 @@ func main() {
 	window.SetKeyCallback(glfw.KeyCallback(closeWindowCallback))
 	gl.Enable(gl.DEPTH_TEST)
 
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadMatrixd(&isometricProjectionMatrix[0])
+	gl.MatrixMode(gl.MODELVIEW)
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.LoadIdentity()
-		gl.Rotatef(50, 1, 1, 0.5)
-		drowPrism(7)
+		drowPrism(4)
 		glfw.WaitEvents()
 		window.SwapBuffers()
 	}
